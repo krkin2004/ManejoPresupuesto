1d10d8646b0620c3905689eb8d7a00b065648fd7
ccarquin <ccarquin@115291556CCH-WS>
2023-04-05 15:13:50 -0400

    Agregar archivos de proyecto.

---
+++ Validaciones/PrimeraLetraMayusculaAtrribute.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManejoPresupuesto.Validaciones
+{
+    public class PrimeraLetraMayusculaAtrribute: ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null || string.IsNullOrEmpty(value.ToString())) 
+            {
+                return ValidationResult.Success;
+            }
+            var primeraletra = value.ToString()[0].ToString();
+
+            if (primeraletra != primeraletra.ToUpper())
+            {
+                return new ValidationResult("La primera letra debe ser mayúscula");
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
